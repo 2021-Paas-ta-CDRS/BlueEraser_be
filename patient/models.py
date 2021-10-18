fecc5ff0ab40182cf5870ef425108392c57ff53b
Junseok Yoon <phobyjun@gmail.com>
2021-10-18 23:16:38 +0900

    FIX: 환자 모델 시리얼라이저 수정

--- patient/models.py
+++ patient/models.py
@@ -3,5 +3,5 @@ from django.db.models.deletion import CASCADE
 from user.models import User
 
 class Patient(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE, db_column='user', primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_column='user', primary_key=True)
     job = models.CharField(max_length=50, null=True)
--- patient/serializer.py
+++ patient/serializer.py
@@ -4,13 +4,15 @@ from user.models import User
 from user.serializers import UserSerializer
 
 class CreatePatientSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer(required=True)
 
     class Meta:
         model = Patient
-        fields = ('user', 'email', 'job')
+        fields = ('user', )
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data, *args, **kwargs):
-        user = User.objects.create_user(validated_data['user']['email'], validated_data['user']['password'])
-        patient = Patient.objects.create(user=user, job='job')
+        user = User.objects.create_user(email=validated_data['user']['email'], password=validated_data['user']['password'])
+        patient = Patient.objects.create(user=user)
+        
+        return patient
