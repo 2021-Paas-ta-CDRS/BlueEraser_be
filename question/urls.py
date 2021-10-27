d7efe0409678afc7293f0ee7b36d5d4d061fd886
Junseok Yoon <phobyjun@gmail.com>
2021-10-27 20:17:52 +0900

    ADD: 문답 API(GET, POST) 추가

--- question/urls.py
+++ question/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuestionFormAPI
+from .views import QuestionFormAPI, QuestionAPI
 
 router = DefaultRouter()
 router.register(r'question_form', QuestionFormAPI)
+router.register(r'', QuestionAPI, basename='question')
 urlpatterns = [
     path('', include(router.urls)),
 ]
