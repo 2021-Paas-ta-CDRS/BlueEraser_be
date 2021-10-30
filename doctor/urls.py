b140ae4653a5983e75857ef1ba7c638cee144aef
Junseok Yoon <phobyjun@gmail.com>
2021-10-30 17:23:42 +0900

    ADD: 상품 관련 API 추가

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CertificateAPI, CreateDoctorAPI, GetDoctorAPI, UpdateDoctorAPI
+from package.views import PackageAPI
 
 router = DefaultRouter()
-router.register(r'', CertificateAPI, basename='certificate')
+router.register(r'certificate', CertificateAPI, basename='certificate')
+router.register(r'package', PackageAPI, basename='package')
 
 urlpatterns = [
     path('signup/', CreateDoctorAPI.as_view()),
     path('update/', UpdateDoctorAPI.as_view()),
     path('', GetDoctorAPI.as_view()),
 
-    path('certificate/', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- package/views.py
+++ package/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from package.models import Package
 
-# Create your views here.
+from package.serializers import PackageSerializer
+
+class PackageAPI(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PackageSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data={'doctor': request.user.doctor, **request.data.dcit()})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=HTTP_201_CREATED, headers=headers)
+    
+    def get_queryset(self):
+        return Package.objects.filter(doctor=self.request.user.doctor)
