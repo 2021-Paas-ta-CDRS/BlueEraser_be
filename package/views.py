1b61fccb4582e810bbdc1f69e7c25bc9d099ed84
Junseok Yoon <phobyjun@gmail.com>
2021-10-30 17:24:16 +0900

    FIX: status 임포트

--- package/views.py
+++ package/views.py
@@ -1,5 +1,5 @@
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from package.models import Package
@@ -15,7 +15,7 @@ class PackageAPI(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
     def get_queryset(self):
         return Package.objects.filter(doctor=self.request.user.doctor)
