15803150ae8b1b778ea6db856de1b4fd11aacafb
Junseok Yoon <phobyjun@gmail.com>
2021-11-01 22:43:49 +0900

    ADD: 매칭, 리뷰 시리얼라이저 추가

--- package/serializers.py
+++ package/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Package
+from .models import Matching, Package, Review
 
 class PackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Package
+        fields = '__all__'
+
+class MatchingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matching
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
         fields = '__all__'
