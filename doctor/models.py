6e2806c7034edc39bdc5cac10a26789a057d55a4
Junseok Yoon <phobyjun@gmail.com>
2021-11-10 21:50:55 +0900

    ADD: 의사 모델 프로필사진 필드 추가

--- doctor/models.py
+++ doctor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields import BooleanField, CharField, TextField
+from django.db.models.fields.files import ImageField
 from user.models import User
 
 class Doctor(models.Model):
@@ -7,6 +8,7 @@ class Doctor(models.Model):
     hospital_address = CharField(max_length=200, verbose_name='병원주소', null=True)
     is_verified = BooleanField(verbose_name='인가여부', null=True, default=False)
     self_pr = TextField(verbose_name='자기소개', null=True)
+    profile_image = ImageField(verbose_name='프로필사진', null=True)
 
 class Certificate(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, db_column='doctor', related_name='certificates')
