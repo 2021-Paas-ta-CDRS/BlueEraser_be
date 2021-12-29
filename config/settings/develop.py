eca8a2b1e2ec25e96f0a2614cb50134b0c011fcf
phobyjun <phobyjun@gmail.com>
2021-12-30 00:05:29 +0900

    FIX: develop db configuration

--- config/settings/develop.py
+++ config/settings/develop.py
@@ -1,6 +1,13 @@
 from .base import *
 
-import dj_database_url
-
 DEBUG = True
-DATABASES['default'] = dj_database_url.parse(get_secret('DATABASE_URL'))
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE"),
+        "HOST": os.environ.get("DB_HOST"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "PORT": os.environ.get("DB_PORT")
+    }
+}
