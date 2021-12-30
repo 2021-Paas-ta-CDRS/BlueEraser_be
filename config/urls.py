a9de407884983d16dc47d457ace1eede8a5f6355
phobyjun <phobyjun@gmail.com>
2021-12-31 01:58:00 +0900

    FIX: admin url

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('user/', include('user.urls')),
     path('patient/', include('patient.urls')),
     path('doctor/', include('doctor.urls')),
